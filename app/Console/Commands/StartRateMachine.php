1913f4db79e6c51083d806c9076d60819810fab0
Sergey Volodko <svolodko@gmail.com>
2019-08-14 07:51:21 +0000

    Add Status field to cdr, Create Artisan Command for run RateMachine

---
+++ app/Console/Commands/StartRateMachine.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\Services\RateMachineService;
+
+class StartRateMachine extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'telco:start-rate-machine {clientId : The ID of client}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Start Rate Machine for Client';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle(RateMachineService $rateMachineService)
+    {
+        $clientId = $this->argument('clientId');
+        $client = \App\Models\Client::find($clientId);
+        if (!$client) {
+            $this->error('Client ' . $clientId . ' is not found');
+            return false;
+        } 
+        if (!$this->confirm('Do you wish to rate the cdr for client ' . $client->name .' ?')) {
+            return true;
+        }
+        $cdrs = \App\Models\Cdr
+            ::where('client_id', $client->id)
+            ->where('done', 0)
+            ->orderBy('id')
+            ->get();
+        
+        $rateMachineService->initMachine($client);
+        foreach ($cdrs as $cdr) {
+            $cdr = $rateMachineService->calculateCall($cdr);
+            $this->info("did: {$cdr->did}, dst:{$cdr->dst}, dur:{$cdr->dur}, cost:{$cdr->cost}");
+        }
+    }
+}
--- app/Services/RateMachineService.php
+++ app/Services/RateMachineService.php
@@ -13,18 +13,56 @@ use \App\Models\Client;
 class RateMachineService
 {
     protected $client;
-
+    protected $coltInvoice;
+    protected $packageIds; // array of telco package id
+    protected $rateId; // id of telco rate
     /**
      * Init machine for Client
      */
     public function initMachine(Client $client) {
         $this->client = $client;
+        $this->coltInvoice = \App\Models\Invoice
+            ::with('invoice_items')
+            ->where('client_id', $client->id)
+            ->where('invoice_category_id', INVOICE_ITEM_CATEGORY_COLT)
+            ->first();
+        $this->packageIds = $this->coltInvoice
+            ->invoice_items->where('product_type', 'telcopackages')
+            ->map(function($item) {
+                return $item->product_id;
+            })->all();
+        $rateItem = $this->coltInvoice
+            ->invoice_items->where('product_type', 'telcorates')->first();
+        $this->rateId = $rateItem ? $rateItem->product_id : null;
     }
 
     /**
      * Calculate cdr row 
      */
     public function calculateCall(Cdr $cdr) {
+        // Looking for rate for dst :
+        $rate = \Modules\Telcorates\Models\TelcorateCode
+            ::where('telcorate_id', $this->rateId)
+            ->whereRaw("instr('{$cdr->dst}', code) = 1")
+            ->orderByRaw('length(code) desc')
+            ->first();
+        if ($rate) {
+            echo "Found rate: code:{$rate->code}, second:{$rate->init_seconds}, increment: {$rate->increment_seconds}, rate:{$rate->rate}" . PHP_EOL;
+        } else {
+            echo 'Rate not found' . PHP_EOL;
+        }
+        // Looking for packages:
+        $package = \Modules\Telcopackages\Models\Telcopackages
+            ::whereIn('id', $this->packageIds)
+            ->whereHas('codes', function($query) use ($cdr) {
+                $query->whereRaw("instr('{$cdr->dst}', code) = 1");
+            })
+            ->first();
+        if ($package) {
+            echo "Found package: name:{$package->name}, minutes:{$package->amount_of_minutes}, price:{$package->price}" . PHP_EOL;
+        } else {
+            echo 'No one Packages found' . PHP_EOL;
+        }
         $cost = rand(0, 75);
 
         $cdr->cost = $cost;
